f0adb9c7e521907057e328412e741fb8e187b746
Carlo Andaya <carlo@carloandaya.com>
2013-08-04 20:51:59 -0700

    CreatedThing class created

---
+++ RedditWP/CreatedThing.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
+
+namespace RedditWP
+{
+    public class CreatedThing : Thing
+    {
+        private Reddit Reddit { get; set; }
+
+        public CreatedThing(Reddit reddit, JToken json)
+            : base(json)
+        {
+            Reddit = reddit;
+            JsonConvert.PopulateObject(json["data"].ToString(), this, reddit.JsonSerializerSettings);
+        }
+
+        [JsonProperty("created")]
+        [JsonConverter(typeof(UnixTimestampConverter))]
+        public DateTime Created { get; set; }
+    }
+}
