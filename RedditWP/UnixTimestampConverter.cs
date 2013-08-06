d9110ec5d706c56c1a14409fe8912b5bcc5428bf
Carlo Andaya <carlo@carloandaya.com>
2013-08-05 23:11:58 -0700

    Completed UnixTimeStampConverter class

---
+++ RedditWP/UnixTimestampConverter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace RedditWP
+{
+    public class UnixTimestampConverter : JsonConverter
+    {
+        public override bool CanConvert(Type objectType)
+        {
+            // UnixTimeStampConverter can only be used on objects that are doubles
+            // or objects that are DateTimes
+            return objectType == typeof(double) || objectType == typeof(DateTime);
+        }
+
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+        {
+            var token = JToken.Load(reader);
+            return Reddit.UnixTimeStampToDateTime(token.Value<double>());            
+        }
+
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+        {
+            writer.WriteValue(value);
+        }
+    }
+}
