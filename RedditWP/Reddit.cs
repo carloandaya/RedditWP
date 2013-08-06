3af1df0c4b5c42ee7a0527ad819c6697e8a39297
Carlo Andaya <carlo@carloandaya.com>
2013-08-05 23:43:31 -0700

    Filling out the Reddit class

--- RedditWP/Reddit.cs
+++ RedditWP/Reddit.cs
@@ -1,12 +1,79 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 
 namespace RedditWP
 {
-    public class Reddit
+    public partial class Reddit
     {
+        #region Constant Urls
+
+        private const string SslLoginUrl = "https://ssl.reddit.com/api/login";
+        private const string LoginUrl = "/api/login/username";
+        private const string UserInfoUrl = "/user/{0}/about.json";
+        private const string MeUrl = "/api/me.json";
+        private const string SubredditAboutUrl = "/r/{0}/about.json";
+        private const string ComposeMessageUrl = "/api/compose";
+        private const string RegisterAccountIrl = "/api/register";
+
+        #endregion
+
+        #region Static Variables
+
+        static Reddit()
+        {
+            UserAgent = "";
+            EnableRateLimit = true;
+            RootDomain = "www.reddit.com";
+        }
+
+        /// <summary>
+        /// Additional values to append to the default RedditSharp user agent.
+        /// </summary>
+        public static string UserAgent { get; set; }
+        /// <summary>
+        /// It is strongly advised that you leave this enabled. Reddit bans excessive
+        /// requests with extreme predjudice.
+        /// </summary>
+        public static bool EnableRateLimit { get; set; }
+        /// <summary>
+        /// The root domain RedditSharp uses to address Reddit.
+        /// www.reddit.com by default
+        /// </summary>
+        public static string RootDomain { get; set; }
+
+        #endregion
+
+        /// <summary>
+        /// The authenticated user for this instance.
+        /// </summary>
+        public AuthenticatedUser User { get; set; }
+
+        internal JsonSerializerSettings JsonSerializerSettings { get; set; }
+
+        private CookieContainer Cookies { get; set; }
+
+        private string AuthCookie { get; set; }
+
+        public Reddit()
+        {
+            JsonSerializerSettings = new JsonSerializerSettings();
+            JsonSerializerSettings.CheckAdditionalContent = false;
+            JsonSerializerSettings.DefaultValueHandling = DefaultValueHandling.Ignore;
+        }
+
+        public AuthenticatedUser LogIn(string username, string password, bool useSsl = true)
+        {
+            throw new NotImplementedException();
+        }
+
+        public RedditUser GetUser(string name)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
