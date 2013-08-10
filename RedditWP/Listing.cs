631b7443f1f4ccbdf1f1dfd20ce365ecb8f78c5c
Carlo Andaya <carlo@carloandaya.com>
2013-08-09 23:56:19 -0700

    Started Listing.cs Started RedditUser.cs

--- RedditWP/Listing.cs
+++ RedditWP/Listing.cs
@@ -3,10 +3,77 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Collections;
 
 namespace RedditWP
 {
-    class Listing
+    public class Listing<T> : IEnumerable<T> where T : Thing
     {
+        private Reddit Reddit { get; set; }
+        private String Url { get; set; }
+
+        internal Listing(Reddit reddit, String url)
+        {
+            Reddit = reddit;
+            Url = url;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return new ListingEnumerator<T>(this);
+        }
+
+        System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+
+        private class ListingEnumerator<T> : IEnumerator<T> where T : Thing
+        {
+            private Listing<T> Listing { get; set; }
+            private int CurrentPageIndex { get; set; }
+            private string After { get; set; }
+            private string Before { get; set; }
+            private Thing[] CurrentPage { get; set; }
+
+            public ListingEnumerator(Listing<T> listing)
+            {
+                Listing = listing;
+                CurrentPageIndex = 0;
+            }
+
+            public T Current
+            {
+                get
+                {
+                    return (T)CurrentPage[CurrentPageIndex - 1];
+                }
+            }
+
+            private void FetchNextPage()
+            {
+
+            }
+
+            object IEnumerator.Current
+            {
+                get { throw new NotImplementedException(); }
+            }
+
+            public bool MoveNext()
+            {
+                throw new NotImplementedException();
+            }
+
+            public void Reset()
+            {
+                throw new NotImplementedException();
+            }
+
+            public void Dispose()
+            {
+                throw new NotImplementedException();
+            }
+        }
     }
 }
--- RedditWP/RedditUser.cs
+++ RedditWP/RedditUser.cs
@@ -3,10 +3,47 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace RedditWP
 {
-    public class RedditUser
+    public class RedditUser : Thing
     {
+        private const string OverviewUrl = "/user/{0}.json";
+        private const string CommentsUrl = "/user/{0}/comments.json";
+        private const string LinksUrl = "/user/{0}/submitted.json";
+        private const string SubscribedSubredditsUrl = "/subreddits/mine.json";
+
+        public RedditUser(Reddit reddit, JToken json)
+            : base(json)
+        {
+            Reddit = reddit;
+        }
+
+        [JsonIgnore]
+        protected Reddit Reddit { get; set; }
+
+        [JsonProperty("name")]
+        public string Name { get; set; }
+        
+        [JsonProperty("is_gold")]
+        public bool HasGold { get; set; }
+        
+        [JsonProperty("is_mod")]
+        public bool IsModerator { get; set; }
+        
+        [JsonProperty("comment_karma")]
+        public int CommentKarma { get; set; }
+
+        [JsonProperty("created")]
+        [JsonConverter(typeof(UnixTimestampConverter))]
+        public DateTime Created { get; set; }
+
+        public Listing<Comment> GetComments()
+        {
+
+        }
+
     }
 }
