faed0fe60c3d4111e1d8eb4d94c7d43e78a7994f
Carlo Andaya <carlo@carloandaya.com>
2013-08-11 12:16:32 -0700

    Almost done with Comment.cs

--- RedditWP/Comment.cs
+++ RedditWP/Comment.cs
@@ -24,8 +24,7 @@ namespace RedditWP
         {
             public HttpWebRequest AsyncRequest { get; set; }
             public HttpWebResponse AsyncResponse { get; set; }
-            public String Message { get; set; }
-            public Comment ReplyComment { get; set; }
+            public Object ParameterValue { get; set; }            
         }
 
         [JsonIgnore]
@@ -105,13 +104,12 @@ namespace RedditWP
             CommentState commentState = new CommentState();
             var request = Reddit.CreatePost(CommentUrl);            
             commentState.AsyncRequest = request;
-            commentState.Message = message;
+            commentState.ParameterValue = message;            
 
             IAsyncResult replyRequestAR = request.BeginGetRequestStream(new AsyncCallback(ReplyRequest), commentState);
             IAsyncResult replyResponseAR = request.BeginGetResponse(new AsyncCallback(ReplyResponse), commentState);
 
-            return returnComment;
-            //return ((CommentState)replyResponseAR.AsyncState).ReplyComment;
+            return returnComment;            
         }
 
         private void ReplyRequest(IAsyncResult ar)
@@ -121,7 +119,7 @@ namespace RedditWP
             Stream stream = request.EndGetRequestStream(ar);
             Reddit.WritePostBody(stream, new
             {
-                text = commentState.Message, 
+                text = (String)commentState.ParameterValue, 
                 thing_id = FullName,
                 uh = Reddit.User.Modhash,
                 api_type = "json"
@@ -138,5 +136,114 @@ namespace RedditWP
             var json = JObject.Parse(data);
             returnComment = new Comment(Reddit, json["json"]["data"]["things"][0]);
         }
+
+        public void Distinguish(DistinguishType distinguishType)
+        {
+            if (Reddit.User == null)
+                throw new Exception("No user logged in.");
+            var request = Reddit.CreatePost(DistinguishUrl);
+            CommentState commentState = new CommentState();
+            commentState.AsyncRequest = request;
+            commentState.ParameterValue = distinguishType;            
+            request.BeginGetRequestStream(new AsyncCallback(DistinguishRequest), commentState);
+        }
+
+        private void DistinguishRequest(IAsyncResult ar)
+        {
+            CommentState commentState = (CommentState)ar.AsyncState;
+            DistinguishType distinguishType = (DistinguishType)commentState.ParameterValue;
+            HttpWebRequest request = commentState.AsyncRequest;
+            Stream stream = request.EndGetRequestStream(ar);
+
+            string how;
+            switch (distinguishType)
+            {
+                case DistinguishType.Admin:
+                    how = "admin";
+                    break;
+                case DistinguishType.Moderator:
+                    how = "yes";
+                    break;
+                case DistinguishType.None:
+                    how = "no";
+                    break;
+                default:
+                    how = "special";
+                    break;
+            }
+
+            Reddit.WritePostBody(stream, new
+            {
+                how,
+                id = Id, 
+                uh = Reddit.User.Modhash
+            });
+
+            request.BeginGetResponse(new AsyncCallback(DistinguishResponse), commentState);
+        }
+
+        private void DistinguishResponse(IAsyncResult ar)
+        {
+            CommentState commentState = (CommentState)ar.AsyncState;
+            HttpWebRequest request = commentState.AsyncRequest;
+            commentState.AsyncResponse = (HttpWebResponse)request.EndGetResponse(ar);
+
+            var data = Reddit.GetResponseString(commentState.AsyncResponse.GetResponseStream());
+            var json = JObject.Parse(data);
+            if (json["jquery"].Count(i => i[0].Value<int>() == 11 && i[1].Value<int>() == 12) == 0)
+                throw new Exception("You are not permitted to distinguish this comment.");
+        }
+
+        public void EditText(string newText)
+        {
+            if (Reddit.User == null)
+                throw new Exception("No user logged in.");
+
+            var request = Reddit.CreatePost(EditUserTextUrl);
+            CommentState commentState = new CommentState();
+            commentState.AsyncRequest = request;
+            commentState.ParameterValue = newText;
+
+            request.BeginGetRequestStream(new AsyncCallback(EditTextRequest), commentState);
+        }
+
+        private void EditTextRequest(IAsyncResult ar)
+        {
+            CommentState commentState = (CommentState)ar.AsyncState;
+            HttpWebRequest request = commentState.AsyncRequest;
+            Stream stream = request.EndGetRequestStream(ar);
+
+            Reddit.WritePostBody(stream, new
+            {
+                api_type = "json",
+                text = (String)commentState.ParameterValue,
+                thing_id = FullName,
+                uh = Reddit.User.Modhash
+            });
+
+            request.BeginGetResponse(new AsyncCallback(EditTextResponse), commentState);
+        }
+
+        private void EditTextResponse(IAsyncResult ar)
+        {
+            CommentState commentState = (CommentState)ar.AsyncState;
+            HttpWebRequest request = commentState.AsyncRequest;
+            commentState.AsyncResponse = (HttpWebResponse)request.EndGetResponse(ar);
+
+            var data = Reddit.GetResponseString(commentState.AsyncResponse.GetResponseStream());
+            JToken json = JToken.Parse(data);
+            if (json["json"].ToString().Contains("\"errors\": []"))
+                Body = (String)commentState.ParameterValue;
+            else
+                throw new Exception("Error editing text.");
+        }
+    }
+
+    public enum DistinguishType
+    {
+        Moderator,
+        Admin, 
+        Special,
+        None
     }
 }
